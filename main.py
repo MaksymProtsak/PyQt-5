dfcae566b1237e4ab3d0b9be8d3e735ce463dac7
Maksym Protsak <maksym.protsak@gmail.com>
2025-06-09 09:27:03 +0300

    Created add_label function. Created main_text, btn in application. Connected btn and all_label function.

--- main.py
+++ main.py
@@ -6,11 +6,28 @@ from PyQt5.QtWidgets import (
     QMainWindow
 )
 
+
+def add_label():
+    print("add")
+
+
 def application():
     app = QApplication(sys.argv)
     window = QMainWindow()
     window.setWindowTitle("First app")
     window.setGeometry(300, 250, 350, 200)
+
+    main_text = QtWidgets.QLabel(window)
+    main_text.setText("This is base text")
+    main_text.move(100, 100)
+    main_text.adjustSize()
+
+    btn = QtWidgets.QPushButton(window)
+    btn.move(70, 150)
+    btn.setText("Press on me")
+    btn.setFixedWidth(200)
+    btn.clicked.connect(add_label)
+
     window.show()
     sys.exit(app.exec_())
 
